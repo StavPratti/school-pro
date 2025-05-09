4a7e45d2c74548fb349af92d4ab67edafb1a55aa
spratth <stavroulaprt@gmail.com>
2025-05-09 21:58:40 +0300

    jakarta

---
+++ src/main/java/gr/aueb/cf/SchoolApplication.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class SchoolApplication extends Application {
+}
